9abc751f3e5e434092cbd737288601b79a2a7982
miguel.villamizar@softwareone.com <miguel.villamizar@softwareone.com>
2024-10-10 16:39:25 -0500

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,17 @@
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+
+
+
+
+
+
